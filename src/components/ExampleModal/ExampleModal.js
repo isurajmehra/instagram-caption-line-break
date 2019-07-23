3a3bf05ff1fe9c074f6428bcb4b045b95e09c6d5
Jaydan <jaydan@anoun.company>
2019-07-23 16:57:43 -0600

    using gatsby-image on example img

--- src/components/ExampleModal/ExampleModal.js
+++ src/components/ExampleModal/ExampleModal.js
@@ -1,25 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 import Dialog, {
   DialogTitle,
   DialogContent,
   DialogFooter,
   DialogButton,
 } from "@material/react-dialog"
-import ExampleModalImage from "../../images/example-modal-iphone-image.png"
 
-
-function ExampleModal(props) {
-    return (
-      <Dialog open={props.open} onClose={props.onClose}>
-        <DialogTitle>Caption Example</DialogTitle>
-        <DialogContent>
-          <img src={ExampleModalImage} width="100%" alt="example caption with line breaks"/>
-        </DialogContent>
-        <DialogFooter>
-          <DialogButton action="dismiss" isDefault>Dismiss</DialogButton>
-        </DialogFooter>
-      </Dialog>
-    )
-  }
+const ExampleModal = (props) => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      avatar: file(absolutePath: { regex: "/example-modal-iphone-image.png/" }) {
+        childImageSharp {
+          fluid(maxWidth:1200) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Dialog open={props.open} onClose={props.onClose}>
+      <DialogTitle>Caption Example</DialogTitle>
+      <DialogContent>
+        <Image
+          fluid={data.avatar.childImageSharp.fluid}
+          alt="example caption with line breaks"
+          style={{
+            minWidth:`300px`
+          }}
+        />
+      </DialogContent>
+      <DialogFooter>
+        <DialogButton action="dismiss" isDefault>
+          Dismiss
+        </DialogButton>
+      </DialogFooter>
+    </Dialog>
+  )
+}
 
 export default ExampleModal;
