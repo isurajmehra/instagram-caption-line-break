30f2d124bf8c737fe98d684b2e6d1885625d0428
Jaydan <jaydan@anoun.company>
2019-07-23 23:43:35 -0600

    config file for SEO

---
+++ config/website.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: `Instagram Caption Line Break Tool`, // Navigation and Site Title
+  description: ``,
+  siteUrl: `https://instagram-caption-line-generator.netlify.com/`, // Domain of your site. No trailing slash!
+  image: "/images/instagram-caption-line-generator-share.png", // Used for SEO
+
+  // Manifest
+  icon: `src/images/favicon.png`, // Used for manifest favicon generation
+  short_name: `Caption Tool`, // shortname for manifest. MUST be shorter than 12 characters
+  owner: "ANOUN", // Author
+  theme_color: `#dc2743`,
+  background_color: `#ffffff`,
+
+  // Social and Analytics
+  twitterUsername: "@anouncompany", // Twitter Username
+  googleAnalyticsID: "",
+}
