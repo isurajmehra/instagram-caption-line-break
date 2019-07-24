3483ce6db1c95333696f0b082ab5b17a226b538a
Jaydan <jaydan@anoun.company>
2019-07-23 23:11:50 -0600

    added support for iOS

--- src/components/CaptionGenerator/CaptionGenerator.js
+++ src/components/CaptionGenerator/CaptionGenerator.js
@@ -13,24 +13,42 @@ class CaptionGenerator extends React.Component {
 
   captionFixAndCopy = () => {
     let inputElement = this.textarea.inputElement
-    if (inputElement.value.length === 0) {
-      return false;
+    let fixedCaption = inputElement.value
+    // this is the magic line that replaces normal line breaks with the invisible separator in UTF
+    fixedCaption = fixedCaption.replace(/(?:\r\n|\r|\n)/g, "\u2063\n")
+    inputElement.value = fixedCaption
+    // // handle iOS as a special case thanks to https://apps4lifehost.com/Instagram/CaptionMaker.html and https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios
+    if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+      // save current contentEditable/readOnly status
+      let editable = inputElement.contentEditable
+      let readOnly = inputElement.readOnly
+      // convert to editable with readonly to stop iOS keyboard opening
+      inputElement.contentEditable = true
+      inputElement.readOnly = true
+      // create selectable range
+      let range = document.createRange()
+      range.selectNodeContents(inputElement)
+      // select range
+      let selection = window.getSelection()
+      selection.removeAllRanges()
+      selection.addRange(range)
+      inputElement.setSelectionRange(0, 999999)
+      // restore contentEditable/readOnly to original state
+      inputElement.contentEditable = editable
+      inputElement.readOnly = readOnly
     } else {
-      let fixedCaption = inputElement.value
-      fixedCaption = fixedCaption.replace(/(?:\r\n|\r|\n)/g, "\u2063\n")
-      inputElement.value = fixedCaption
       inputElement.select()
-      document.execCommand("copy")
-      this.setState({
-        isSnackbarOpen: true,
-      })
     }
+    document.execCommand("copy")
+    this.setState({
+      isSnackbarOpen: true,
+    })
   }
-
+  // reset snackbar to original state
   closeSnackbar = () => {
     this.setState({
-        isSnackbarOpen: false,
-      })
+      isSnackbarOpen: false,
+    })
   }
 
   render() {
