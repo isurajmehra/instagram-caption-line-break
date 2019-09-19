3f8804107bc8778306a068ca896facff2dc56146
Urwin <jurwin1@BOIL160701e28.local>
2019-09-19 16:12:39 -0600

    added comment

--- src/components/CaptionGenerator/CaptionGenerator.js
+++ src/components/CaptionGenerator/CaptionGenerator.js
@@ -43,7 +43,7 @@ class CaptionGenerator extends React.Component {
     } else {
       el.select()
     }
-
+    // run the copy command and open the snackbar 
     document.execCommand("copy")
     this.setState({
       isSnackbarOpen: true,
